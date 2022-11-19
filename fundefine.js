9fffc293c6b402c65da34480fd2da551f135afd5
patilbhakti97 <114335887+patilbhakti97@users.noreply.github.com>
2022-11-19 15:58:42 +0530

    function define

---
+++ fundefine.js
@@ -0,0 +1,20 @@
+// define by expresion
+let k=function fun()
+{
+    console.log("Hi");
+}
+k()
+
+// anonymous
+let Bhakti=function()
+{
+    console.log("Hello");
+}
+Bhakti()
+
+// Arrow function
+let A=()=>
+{
+    console.log("Hello world");
+}
+A()
